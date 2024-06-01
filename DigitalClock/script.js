48cd97594b29c3d9d774a8b4ab10ef652b7d2fff
VipulRanjan29 <139875544+VipulRanjan29@users.noreply.github.com>
2024-06-01 13:37:10 +0000

    Clock done & new Home Button

---
+++ DigitalClock/script.js
@@ -0,0 +1,9 @@
+const clock = document.querySelector('.clock');
+
+setInterval(function(){
+    let date = new Date();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+    clock.innerHTML = `${hour}:${minute}:${second}`;
+},1000);
